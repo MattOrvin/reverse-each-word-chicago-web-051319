70fb34df694f75c636aecf672c2f5722b6cbac2e
Matt Orvin <matthew.orvin@gmail.com>
2019-04-17 21:19:03 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,6 @@
 def reverse_each_word(sentence)
-  sentence.split.collect do |reversed_sentence|
-  new_sentence = 
-    new_sentence << reversed_sentence.reverse
+  new_sentence = sentence.split.collect do |reversed_sentence|
+    reversed_sentence.reverse
 end
 new_sentence.join
 end
